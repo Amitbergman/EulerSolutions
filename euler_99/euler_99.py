18268e4721af849835c1cffe4aa653c76e06ce07
Amit Bergman <ambergma@microsoft.com>
2020-11-21 14:15:47 +0200

    changes

--- euler_99/euler_99.py
+++ euler_99/euler_99.py
@@ -11,20 +11,18 @@ l_2 = list()
 a = 0
 for x in f:
     splitted = x.split(",")
-
     l_1.append((int(splitted[0]), int(splitted[1]), 0))
     l_2.append((int(splitted[0]), int(splitted[1]), 0))
 
+maxer_ind = 0
+maxer = l_1[0][2]
 for j in range (100):
     for i in range(len(l_1)):
         loger = math.log(l_1[i][0], 2)
         l_1[i] = (l_1[i][0], l_1[i][1], l_1[i][1] * loger)
-maxer_ind = 0
-maxer = l_1[0][2]
-for i in range(len(l_1)):
-    if (l_1[i][2] > maxer):
-        maxer_ind = i
-        maxer = l_1[i][2]
+        if (l_1[i][2] > maxer):
+            maxer_ind = i
+            maxer = l_1[i][2]
 
 print("The solution is: " + str(maxer_ind+1))
 
