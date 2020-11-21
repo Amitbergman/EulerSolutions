880d75fa8d591cbb092bd18d24102adf276ae8da
Amit Bergman <ambergma@microsoft.com>
2020-11-21 14:28:18 +0200

    changes

--- euler_99/euler_99.py
+++ euler_99/euler_99.py
@@ -6,25 +6,18 @@ import time
 import math
 t0 = time.perf_counter()
 
-l_1 = list()
-l_2 = list()
-a = 0
-for x in f:
-    splitted = x.split(",")
-    l_1.append((int(splitted[0]), int(splitted[1]), 0))
-    l_2.append((int(splitted[0]), int(splitted[1]), 0))
-
+index = 0
 maxer_ind = 0
-maxer = l_1[0][2]
-for j in range (100):
-    for i in range(len(l_1)):
-        loger = math.log(l_1[i][0], 2)
-        l_1[i] = (l_1[i][0], l_1[i][1], l_1[i][1] * loger)
-        if (l_1[i][2] > maxer):
-            maxer_ind = i
-            maxer = l_1[i][2]
+maxer = 1
+for line in f:
+    index = index + 1
+    splitted_line = line.split(",")
+    currentValue  = math.log(int(splitted_line[0]), 2) * int(splitted_line[1])
+    if (currentValue > maxer):
+        maxer_ind = index
+        maxer = currentValue 
 
-print("The solution is: " + str(maxer_ind+1))
+print("The solution is: " + str(maxer_ind))
 
 print(f'Completed in {(time.perf_counter() - t0) * 1000:0.3f} ms')
 
